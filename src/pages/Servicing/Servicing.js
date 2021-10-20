06a80bcb6eb5147d529aa4744b103c6e7ac79b5b
Ali Akkas <aliakkas006@gmail.com>
2021-10-20 18:52:11 +0600

    details added

--- src/pages/Servicing/Servicing.js
+++ src/pages/Servicing/Servicing.js
@@ -1,12 +1,26 @@
 
+import { useEffect } from 'react';
 import { useParams } from 'react-router';
+import { useState } from 'react/cjs/react.development';
 
 const Servicing = () => {
-
     const { id } = useParams();
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('/doctorsData.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+    
+    const exactItem = services.find(service => service.id === id);
+
     return (
         <div className="text-center text-2xl text-danger mx-16">
             <h2>This is servicing id: {id} </h2>
+            <img src={exactItem?.img} alt="" />
+            <h2>Name: {exactItem?.name} </h2>
+            <p>Description: {exactItem?.description} </p>
         </div>
     );
 };
